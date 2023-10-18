f034eef03597b41c9b9f43aff60271d5ebed2467
emhedge <122066271+emhedge@users.noreply.github.com>
2023-10-18 16:01:32 +0800

    Reversed a string with an array and for loop
    Split the string into an array, then return each array value using a
    for loop beginning with textArray.length - 1 and a negative i-- iterator

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    text = '';
+    let textArray = text.split(''); 
+    let reversedText = '';
 
+    for (i = textArray.length - 1; i >= 0; i--) {
+        reversedText += textArray[i];
+    }
+    return reversedText;
 };
+
+reverseString();
 
 // Do not edit below this line
 module.exports = reverseString;
