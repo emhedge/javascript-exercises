89ec1bb00776b270481307bdd6f12f06855d4767
emhedge <122066271+emhedge@users.noreply.github.com>
2023-10-19 12:11:37 +0800

    Used ...args to remove args from remove array
    Had to reference the solution for this example, and left my commented-
    out code from my initial attempt for historical reference, despite it
    being against 'clean code' standards. Next commit will remove it.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,28 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
 
+    let newArray = [];
+
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    }); return newArray;
+    
+    // let removedItems = Array.from(args);
+    
+    // for (i = 0; i < array.length; i++){
+    //     if (removedItems === array[i]) {
+    //         array.slice(args[i - 1], 1);
+    //         return newArray;
+    //     }  else continue;
+        
+    // }
+    
+    
+    
 };
+
+removeFromArray([1, 2, 3, 4], 3)
 
 // Do not edit below this line
 module.exports = removeFromArray;
