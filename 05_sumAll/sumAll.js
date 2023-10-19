ac16565fefa1e2c680c2f32aa3d9a42f166d9c30
emhedge <122066271+emhedge@users.noreply.github.com>
2023-10-19 13:27:34 +0800

    Very messy first successful sumAll function
    Though it's a bit of a mess, I passed all 6 tests. Next, I'll review
    the solution and clean up the code a bit.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,32 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
 
-};
+    let finalSum = Number('');
+    let args = [first, second];
+    args.sort(function(a, b){return a - b});
+    
+    if ((Math.sign(first) < 0) || (Math.sign(second) < 0)) {
+        return "ERROR"
+    } 
+
+    if (((typeof(first) === "string")) || ((typeof(second) === "string"))) {
+        return "ERROR"
+    }
+
+    if ((typeof(first) === "object") || (typeof(second) === "object")) {
+        return "ERROR"
+    }
+
+    
+    let i = args[0];
+
+    while (i <= args[1] && i  ) {
+        finalSum += i;
+        i++
+}; return finalSum;
+}
+
+
+sumAll(10, [90, 1]);
 
 // Do not edit below this line
 module.exports = sumAll;
