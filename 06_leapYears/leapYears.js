15ca54cdc9c9b7a4c204e25f8e1092ae4745529a
emhedge <122066271+emhedge@users.noreply.github.com>
2023-10-20 15:06:11 +0800

    Created a function to test leap year/not
    Using if statements and the && operator, passed all five tests.
    Next I'll compare to TOP solution and streamline my function.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(givenYear) {
+    if (givenYear % 400 == 0) return true;
+    else if (((givenYear % 4) == 0) && ((givenYear % 100) == 0)) return false;
+    else if ((givenYear % 4) == !0) return false;
+    else if (givenYear % 4 == 0) return true;
 };
+
+leapYears(700);
 
 // Do not edit below this line
 module.exports = leapYears;
