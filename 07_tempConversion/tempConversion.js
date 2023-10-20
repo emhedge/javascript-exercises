77fdd59d239fa5a54347ee001ac4266981d8be0a
emhedge <122066271+emhedge@users.noreply.github.com>
2023-10-20 15:25:54 +0800

    Created a C/F temp conversion function via toFixed()
    Used toFixed to round decimals to 1 point, Number() to ensure strings
    are converted to numbers.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  // temp in F = (temp - 32) * (5 / 9)C
+  temp = (temp - 32) * (5 / 9);
+  if (temp % 1 !== 0) {
+    return Number(temp.toFixed(1));
+  } else return temp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  // temp in C = (temp * (9/5) + 32)F
+  temp = (temp * (9 / 5) + 32)
+  if (temp % 1 !== 0) {
+    return Number(temp.toFixed(1));
+  } else return temp;
 };
 
 // Do not edit below this line
